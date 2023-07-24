efe4031759fa474afe39aed6718a636fd7023314
gabrieldeolaguibel <99414447+gabrieldeolaguibel@users.noreply.github.com>
2023-07-23 23:46:35 -0500

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavbarComp from './component/navigation/NavbarComp';
+import AuthProvider from './context/AuthContext';  // <-- Importing the AuthProvider context
 
 function App() {
   return (
     <div className="App">
-      <NavbarComp/>
+      <AuthProvider> {/* Wrapping the components with the AuthProvider */}
+        <NavbarComp/>
+      </AuthProvider>
     </div>
   );
 }
